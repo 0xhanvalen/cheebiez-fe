96c4cfda303b23db2422893e21c3acb5ddcfbbe8
Mack Bowes <mackenziepbowes@gmail.com>
2022-03-25 13:28:36 -0700

    updated site

--- components/RandomCheeb.js
+++ components/RandomCheeb.js
@@ -1,10 +1,14 @@
+import {useState} from 'react';
 import {Image} from '@chakra-ui/react';
 
 export const RandomCheeb = () => {
 
-    const cheebIndex = Math.ceil(Math.random() * 12);
+    const getCheebIndex = () => {
+        return Math.ceil(Math.random() * 12)
+    }
+    const [cheebIndex, setCheebIndex] = useState(getCheebIndex());
 
     return (
-        <Image src={`/images/Cheeb${cheebIndex}.png`} alt="A Cheeb" />
+        <Image src={`/images/old/Cheeb${cheebIndex}.png`} alt="A Cheeb" onClick={() => setCheebIndex(getCheebIndex())}/>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,58 @@
+import {useState, useEffect} from 'react';
 import Head from 'next/head'
 import {Box, Image} from '@chakra-ui/react';
 import { RandomCheeb } from '../components/RandomCheeb';
 
 export default function Home() {
+  const [vw, setVW] = useState(1920);
+  const [vh, setVH] = useState(1080);
+
+
+  const useMousePosition = () => {
+    const [mousePosition, setMousePosition] = useState({x: null, y: null});
+
+    const updateMousePosition = ev => {
+      setMousePosition({ x: ev.clientX, y: ev.clientY});
+    };
+
+    useEffect(() => {
+      window.addEventListener("mousemove", updateMousePosition);
+
+      return () => window.removeEventListener("mousemove", updateMousePosition);
+    }, []);
+
+    return mousePosition;
+  };
+
+  useEffect(() => {
+    setVW(window?.innerWidth);
+    setVH(window?.innerHeight);
+  },[]);
+
+  const {x,y} = useMousePosition();
+
   return (
     <>
       <Head>
-        <title>Cheebiez</title>
+        <title>COMING SOON | Cheebiez</title>
       </Head>
-      <Box sx={{width: `100vw`, minHeight: `100vh`, backgroundColor: `#FFE3E4`, backgroundImage: `/images/BG.png`}}>
-        <Image sx={{display: [`none`, `block`], position: `absolute`, top: `50%`, left: `50%`, width: `100vw`, height: `100vh`, transform: `translate(-50%, -50%)`}} src="/images/Border.png" alt="The Cheebiez Themselves"/>
-        <Image sx={{position: `absolute`, top: `50%`, left: `50%`, maxWidth: [`90%`, `33vw`], maxHeight: `33vh`, transform: `translate(-50%, -50%)`}} src="/images/Cheebs-logo.png" alt="Cheebiez"/>
-        <Box sx={{display: [`block`, `none`], position: `absolute`, bottom: `0`, left: `50%`, transform: `translateX(-50%)`}}>
-          <RandomCheeb />
+      <Box sx={{width: `100vw`, minHeight: `100vh`, backgroundImage: `/images/Cheebz_Website_Background.png`, overflow: `hidden`}}>
+        <Image src="/images/Cheebz_Website_Stars1.png" sx={{display: [`none`, `block`],width: `100vw`, height: `100vh`, position: `absolute`, overflow: `hidden`, transform: `translateX(${(x - (vw/2))/100}px) translateY(${(y - (vh/2))/100}px)`}}/>
+        <Image src="/images/Cheebz_Website_TwinklingStars.png" sx={{display: [`none`, `block`],width: `100vw`, height: `100vh`, position: `absolute`, overflow: `hidden`, transform: `translateX(${(x - (vw/2))/80}px) translateY(${(y - (vh/2))/80}px)`}}/>
+        <Image src="/images/Cheebz_Website_Cheebiez.png" sx={{display: [`none`, `block`],width: `100vw`, height: `100vh`, position: `absolute`, overflow: `hidden`, transform: `translateX(${(x - (vw/2))/60}px) translateY(${(y - (vh/2))/60}px)`}}/>
+        <Image src="/images/Cheebz_Website_globe.png" sx={{display: [`none`, `block`],width: `100vw`, height: `100vh`, position: `absolute`, overflow: `hidden`, transform: `scale(1.05) rotate(${x * (2*360/vw)}deg) translateX(${(x - (vw/2))/55}px) translateY(${(y - (vh/2))/55}px)`}}/>
+        <Image src="/images/Cheebz_Website_Logo.png" sx={{display: [`none`, `block`],width: `100vw`, height: `100vh`, position: `absolute`, overflow: `hidden`, left: `0`, transform: `translateX(${-(x - (vw/2))/30}px) translateY(${-(y - (vh/2))/30}px)`}}/>
+        <Image src="/images/Cheebz_Website_ComingSoon.png" sx={{display: [`none`, `block`],width: `100vw`, height: `100vh`, position: `absolute`, overflow: `hidden`}}/>
+        <Box sx={{display: [`flex`, `none`], flexDirection: `column`, alignItems: `center`, width: `100vw`, height: `100vh`, justifyContent: `space-between`}}>
+          <Box sx={{display: `grid`, placeItems: `center`, width: `100vw`, height: `50vh`, color: `white`}}>
+          <Image src="/images/Cheebz_Website_Cheebiez.png" sx={{width: `100vw`, position: `absolute`, overflow: `hidden`}}/>
+          <Image src="/images/Cheebz_Website_globe.png" sx={{width: `100vw`, position: `absolute`, overflow: `hidden`}}/>
+        <Image src="/images/Cheebz_Website_Logo.png" sx={{width: `100vw`, position: `absolute`, overflow: `hidden`}}/>
+          </Box>
+          <Box><RandomCheeb /></Box>
         </Box>
       </Box>
+      {/* <Box sx={{position: `absolute`, backgroundColor: `white`, color: `black`, padding: `1ex 1em`, border: `3px solid black`, left: `0`, top: `0`}}>{x}, {y}</Box> */}
     </>
   )
 }
