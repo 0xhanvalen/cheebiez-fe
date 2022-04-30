4a08ff3c072ea2130db46e8cdbc95f960cdab4e0
Mack Bowes <mackenziepbowes@gmail.com>
2022-04-30 09:42:10 -0700

    updated cheebz logo

--- components/SpinningGlobe/SpinningGlobe.js
+++ components/SpinningGlobe/SpinningGlobe.js
@@ -5,26 +5,56 @@ import styles from "./SpinningGlobe.module.scss";
 export const SpinningGlobe = () => {
   const globeSpinKeyframes = keyframes`
    0% { transform: rotate(0deg);}
-   100% { transform: rotate(360deg);}
+   100% { transform: rotate(-360deg);}
    `;
 
   const globeSpinAnimation = `${globeSpinKeyframes} 20s linear infinite`;
 
   const lineArtSpinKeyframes = keyframes`
   0% { transform: rotate(0deg);}
-  100% { transform: rotate(-360deg);}
+  100% { transform: rotate(360deg);}
+  `;
+
+  const cheebiezSpin = keyframes`
+  0% { transform: rotate(0deg);}
+  50% { transform: translateX(-15px) rotate(180deg);}
+  100% { transform: rotate(360deg);}
   `;
 
   const lineArtSpinAnimation = `${lineArtSpinKeyframes} 60s linear infinite`;
+  const cloudsSpinAnimation = `${lineArtSpinKeyframes} 80s linear infinite`;
+  const CheebiezAnimation = `${cheebiezSpin} 15s linear infinite`;
 
   return (
     <>
       <Box className={styles.container} draggable="false">
         <Image
           as={motion.img}
+          animation={cloudsSpinAnimation}
+          className={styles.clouds}
+          src="/images/GlobeAssets/Clouds-01.png"
+          alt=""
+          draggable="false"
+        />
+        <Image
+          as={motion.img}
           animation={lineArtSpinAnimation}
-          className={styles.lineArt}
-          src="/images/GlobeAssets/GlobeLineArt.png"
+          className={styles.buildings}
+          src="/images/GlobeAssets/Buildings-01.png"
+          alt=""
+          draggable="false"
+        />
+        <Image
+          className={styles.atmosphere}
+          src="/images/GlobeAssets/Atmospheric_Perspective-01.png"
+          alt=""
+          draggable="false"
+        />
+        <Image
+          as={motion.img}
+          animation={CheebiezAnimation}
+          className={styles.cheebiez}
+          src="/images/GlobeAssets/Cheebiez-01.png"
           alt=""
           draggable="false"
         />
@@ -32,27 +62,25 @@ export const SpinningGlobe = () => {
           as={motion.img}
           animation={globeSpinAnimation}
           className={styles.globe}
-          src="/images/GlobeAssets/Globe.png"
+          src="/images/GlobeAssets/Globe-01.png"
           alt=""
           draggable="false"
         />
         <Image
           className={styles.highlight}
-          src="/images/GlobeAssets/Highlight.png"
+          src="/images/GlobeAssets/Highlight-01.png"
           alt=""
           draggable="false"
         />
         <Image
           className={styles.shadow}
-          src="/images/GlobeAssets/Shadow.png"
+          src="/images/GlobeAssets/Shadow-01.png"
           alt=""
           draggable="false"
         />
         <Image
-          as={motion.img}
-          animation={globeSpinAnimation}
           className={styles.globe}
-          src="/images/GlobeAssets/GlobeOutline.png"
+          src="/images/GlobeAssets/Lineart-01.png"
           alt=""
           draggable="false"
         />
